995dfe66ad5f99f942a1eb61a0eceae73a1649f5
Aurélien Richez <aurelien.richez@fabernovel.com>
2018-04-27 12:47:10 +0200

    base with metadata-extractor

--- server/heybro/project/Dependencies.scala
+++ server/heybro/project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val metadataExtractor = "com.drewnoakes" % "metadata-extractor" % "2.11.0"
 }
---
+++ server/heybro/src/main/scala/heybro/exif/GpsDataExtractor.scala
@@ -0,0 +1,29 @@
+package heybro.exif
+
+import java.io.{File, FileInputStream, InputStream}
+
+import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.exif.GpsDirectory
+
+import scala.util.Try
+
+object GpsDataExtractor {
+
+  case class GeoLoc(lat: Double, long: Double)
+
+  def getGeoLocation(file: InputStream) = {
+    val metadataTry = Try(ImageMetadataReader.readMetadata(file))
+
+
+    for(metadata <- metadataTry) yield {
+      Option(metadata.getFirstDirectoryOfType(classOf[GpsDirectory]))
+        .flatMap(dir => Option(dir.getGeoLocation))
+        .map(geo => GeoLoc(geo.getLatitude, geo.getLongitude))
+    }
+  }
+
+  def getGeoLocation(file: File) : Try[Option[GeoLoc]] = {
+    Try(new FileInputStream(file)).flatMap(getGeoLocation)
+  }
+
+}
